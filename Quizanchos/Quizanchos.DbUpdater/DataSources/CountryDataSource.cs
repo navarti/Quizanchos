c42ad065fb71f88f74b4c715535175effb135c4c
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-09-08 21:37:16 +0200

    Fixed parsing of json

--- Quizanchos/Quizanchos.DbUpdater/DataSources/CountryDataSource.cs
+++ Quizanchos/Quizanchos.DbUpdater/DataSources/CountryDataSource.cs
@@ -34,19 +34,53 @@ internal class CountryDataSource
 
     private void ProcessTokenAndAppend(JToken token)
     {
-        try
-        {
-            string name = token["name"]["common"].ToString();
-            float area = token["area"].ToObject<float>();
-            int population = token["population"].ToObject<int>();
+        string name = GetName(token);
+        float area = GetArea(token);
+        int population = GetPopulation(token);
+            
+        Country country = new Country(name, area, population);
+        _countries.Add(country);
+    }
 
-            Country country = new Country(name, area, population);
-            _countries.Add(country);
-        }
-        catch(NullReferenceException)
+    private string GetName(JToken token)
+    {
+        const string TokenName = "name";
+        const string TokenCommon = "common";
+
+        string? name = token[TokenName]?[TokenCommon]?.ToString();
+        if(name is null)
         {
-            Console.WriteLine("Could not parse attributes of JToken");
-            throw;
+            ThrowAttribute($"{TokenName}.{TokenCommon}");
         }
+        return name;
+    }
+
+    private float GetArea(JToken token)
+    {
+        const string TokenArea = "area";
+
+        float? area = token[TokenArea]?.ToObject<float>();
+        if (area is null)
+        {
+            ThrowAttribute(TokenArea);
+        }
+        return area.Value;
+    }
+
+    private int GetPopulation(JToken token)
+    {
+        const string TokenPopulation = "population";
+
+        int? population = token[TokenPopulation]?.ToObject<int>();
+        if (population is null)
+        {
+            ThrowAttribute(TokenPopulation);
+        }
+        return population.Value;
+    }
+
+    private void ThrowAttribute(string attributeName)
+    {
+        throw new ArgumentNullException($"Token does not have attribute {attributeName}");
     }
 }
