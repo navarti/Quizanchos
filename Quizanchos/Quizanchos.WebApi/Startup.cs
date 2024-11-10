f2a2aea0da376e3939a899427e51c9ccf8332d22
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-11-10 18:30:02 +0100

    Configured startup

--- Quizanchos/Quizanchos.WebApi/Startup.cs
+++ Quizanchos/Quizanchos.WebApi/Startup.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Quizanchos.Domain;
+using Quizanchos.Domain.Entities;
 using Quizanchos.Domain.Repositories.Interfaces;
 using Quizanchos.Domain.Repositories.Realizations;
-using Quizanchos.Domain;
+using Quizanchos.WebApi.Extensions;
 using Quizanchos.WebApi.Services.Interfaces;
 using Quizanchos.WebApi.Services.Realizations;
 using Quizanchos.WebApi.Util;
-using Quizanchos.WebApi.Extensions;
+using System.Text;
 
 namespace Quizanchos.WebApi;
 
@@ -29,6 +34,7 @@ public static class Startup
         app.UseStaticFiles();
 
         app.UseAuthorization();
+        app.UseAuthorization();
 
         app.UseRouting();
 
@@ -38,6 +44,42 @@ public static class Startup
             pattern: "{controller=Home}/{action=Index}/{id?}");
     }
 
+    public static void AddAuthorizaiton(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+        })
+            .AddEntityFrameworkStores<QuizDbContext>()
+            .AddDefaultTokenProviders();
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ClockSkew = TimeSpan.Zero,
+
+                ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+            };
+        });
+    }
 
     public static void AddApplicationServices(this WebApplicationBuilder builder)
     {
@@ -52,6 +94,9 @@ public static class Startup
         });
 
         builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+        builder.Services.AddTransient<IJwtService, JwtService>();
+        builder.Services.AddTransient<IQuizAuthorizationService, QuizAuthorizationService>(); 
 
         builder.Services.AddTransient(typeof(IEntityRepository<,>), typeof(EntityRepositoryBase<,>));
 
