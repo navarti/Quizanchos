c55952b9a0bd70bb5598d3df73d25cafd3b02e13
navarti <danil2005oleg@gmail.com>
2026-02-02 22:51:34 +0100

    Fixed migrations

---
+++ Quizanchos/Quizanchos.Domain/Configurations/Quiz/ModelBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Quizanchos.Domain.Configurations.Quiz;
+
+internal static class ModelBuilderExtensions
+{
+    public static void ApplyQuizConfiguration(this ModelBuilder modelBuilder)
+    {
+        // Core domain configurations
+        modelBuilder.ApplyConfiguration(new ApplicationUserConfiguration());
+        modelBuilder.ApplyConfiguration(new GameSessionConfiguration());
+        modelBuilder.ApplyConfiguration(new GameSessionPlayerConfiguration());
+
+        // Quiz configurations
+        modelBuilder.ApplyConfiguration(new QuizEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new QuizCategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new FeatureCommonConfiguration());
+        modelBuilder.ApplyConfiguration(new FeatureIntConfiguration());
+        modelBuilder.ApplyConfiguration(new FeatureFloatConfiguration());
+        modelBuilder.ApplyConfiguration(new QuizCardFloatConfiguration());
+        modelBuilder.ApplyConfiguration(new QuizCardIntConfiguration());
+
+        // Quiz game session configurations
+        modelBuilder.ApplyConfiguration(new QuizGameSessionStateConfiguration());
+        modelBuilder.ApplyConfiguration(new QuizSessionCardConfiguration());
+        modelBuilder.ApplyConfiguration(new QuizSessionCardAnswerConfiguration());
+        modelBuilder.ApplyConfiguration(new QuizSessionPlayerScoreConfiguration());
+    }
+}
